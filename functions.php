b4d6b669b6ea8802d394c751f0676f10226f7191
ruwaisee <fadil.shaaban@gmail.com>
2021-10-25 01:59:03 -0400

    fixing format

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  * Enqueue scripts and styles.
  */
 function shaban_scripts() {
-	wp_enqueue_style( 'child-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'shaban-style', get_stylesheet_uri(), NULL, microtime());
 	
 		wp_enqueue_style( 'shaban-contnt-sidebar', get_template_directory_uri() . '/layouts/content-sidebar.css' );
 		
@@ -21,8 +21,8 @@ wp_enqueue_script( 'shaban-superfish-settings', get_template_directory_uri() . '
 
 	wp_enqueue_script( 'shaban-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {˚¥
+		wp_enqueue_script( 'comment-reply');
 	}
 }
 add_action( 'wp_enqueue_scripts', 'shaban_scripts' );
